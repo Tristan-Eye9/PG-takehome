7afc7ba1303f03e951873d1c7cb99a3c471b7bdd
Tristan-Eye9 <tecpetit@gmail.com>
2025-11-25 17:29:44 -0500

    Adjusted for use with Intraday Documentation

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const fetch = require('node-fetch');
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
-// Simple test route
+// DELETE ME LATER
+// Simple test route 
 app.get('/', (req, res) => {
     res.json({ message: 'Express is working!' });
 });
 
+// Intraday API usage
+// TODO: Implement Intraday API route
+
+
+
+
+// Listen here
 app.listen(PORT, () => {
   console.log(`Server running on httplocalhost${PORT}`);
 });
