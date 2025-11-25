378fb805de2df69fd7fc4f9e8e7bff21bd126b32
Tristan-Eye9 <tecpetit@gmail.com>
2025-11-25 18:09:02 -0500

    Basic intraday API implementation

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const fetch = require('node-fetch');
 const app = express();
 const PORT = 3001;
+//Use environment variables for API key security
+require('dotenv').config();
+const apiKey = process.env.ALPHAVANTAGE_API_KEY;
 
 // DELETE ME LATER
 // Simple test route 
@@ -10,12 +12,34 @@ app.get('/', (req, res) => {
 });
 
 // Intraday API usage
-// TODO: Implement Intraday API route
 
+// Intraday API Route
+// Example usage: http://localhost:3001/api/intraday/IBM
+app.get('/api/intraday/:symbol', async(req, res) => {
+  const {symbol} = req.params;
+  //URL using params similar to documentation
+  const url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=15min&apikey=${apiKey}`;
 
+//Try to fetch the request
+try {
+  const response = await fetch(url);
 
+  //If the fetch fails
+  if (!response.ok){
+    return res.status(response.status).json({error: 'API request failed'});
+  }
+
+  //otherwise Await a response from the url
+  const data = await response.json();
+  res.json(data);
+} //Otherwise catch any errors
+catch (issue){
+  console.error(issue);
+  res.status(500).json({error: 'Server error fetching data'});
+}
+});
 
 // Listen here
 app.listen(PORT, () => {
-  console.log(`Server running on httplocalhost${PORT}`);
+  console.log(`Server running on http://localhost${PORT}`);
 });
