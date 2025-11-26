8cc35573867a6b0e4c07766e37d64c96e2b6cac4
Tristan-Eye9 <tecpetit@gmail.com>
2025-11-26 13:14:27 -0500

    Midpoint on developing API translation to specs

--- dotNet_implementation/Program.cs
+++ dotNet_implementation/Program.cs
@@ -1,4 +1,7 @@
-using DotNetEnv;
+
+using System.Globalization;
+using System.Text.Json; //For simple Json Parsing
+using DotNetEnv; // For importing .env variables (my api key)
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,7 +22,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+
+
 app.MapGet("/api/intraday/{symbol}", async (string symbol) => {
+    var timeInterval = "15min"; // Hardcoded due to specifications, but can be changed for testing
     var apiKey = Environment.GetEnvironmentVariable("ALPHAVANTAGE_API_KEY");
     if (string.IsNullOrWhiteSpace(apiKey)) 
         return Results.BadRequest(new
@@ -27,7 +33,7 @@ app.MapGet("/api/intraday/{symbol}", async (string symbol) => {
             error = "Missing ALPHAVANTAGE_API_KEY"
         });
     
-    var url = $"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={symbol}&interval=15min&outputsize=compact&apikey={apiKey}";
+    var url = $"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={symbol}&interval={timeInterval}&outputsize=compact&apikey={apiKey}";
     using var http = new HttpClient();
     var response = await http.GetAsync(url);
 
@@ -38,6 +44,28 @@ app.MapGet("/api/intraday/{symbol}", async (string symbol) => {
 
     //TODO: Parse the response
 
+    //Set up map
+    using var preParse = JsonDocument.Parse(json);
+    var root = preParse.RootElement;
+
+    // If supported timeInterval, populate timeSeries
+    if (!root.TryGetProperty($"Time Series ({timeInterval})", out var timeSeries))
+        return Results.BadRequest(new { error = "No time series data found" });
+
+    //build tuples
+    var data = new List<(decimal low, decimal high, long volume)>();
+
+    foreach (var i in timeSeries.EnumerateObject())
+    {
+        var entry = i.Value;
+        
+        var low = decimal.Parse(entry.GetProperty("3. low").GetString()!, CultureInfo.InvariantCulture);
+        var high = decimal.Parse(entry.GetProperty("2. high").GetString()!, CultureInfo.InvariantCulture);
+        var volume = long.Parse(entry.GetProperty("5. volume").GetString()!, CultureInfo.InvariantCulture);
+
+        data.Add((low, high, volume));
+    }
+    
     return Results.Text(json, "application/json");
 
 });
