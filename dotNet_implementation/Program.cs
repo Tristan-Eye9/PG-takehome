74597787628c8180f86a8d42babce5943353320a
Tristan-Eye9 <tecpetit@gmail.com>
2025-11-26 20:45:12 -0500

    Documentation overhaul

--- dotNet_implementation/Program.cs
+++ dotNet_implementation/Program.cs
@@ -4,15 +4,18 @@ using DotNetEnv; // For importing .env variables (my api key)
 var builder = WebApplication.CreateBuilder(args);
 
 // Load .env file into environment variables
+// Note: I prefer using .env files for a variety of reasons, but generally
+// they're the easiest way to implement security in early development. And
+// Much easier than constantly changing congifuration files.
 Env.Load();
+
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+if (app.Environment.IsDevelopment()){
     app.MapOpenApi();
 }
 
@@ -23,8 +26,7 @@ app.MapGet("/api/intraday/{symbol}", async (string symbol) => {
     var outputSize = "compact"; // Full would meet the specifications, but since it requires premium, I'm using compact.
     var apiKey = Environment.GetEnvironmentVariable("ALPHAVANTAGE_API_KEY");
     if (string.IsNullOrWhiteSpace(apiKey)) 
-        return Results.BadRequest(new
-        {
+        return Results.BadRequest(new{
             error = "Missing ALPHAVANTAGE_API_KEY"
         });
     
@@ -39,7 +41,7 @@ app.MapGet("/api/intraday/{symbol}", async (string symbol) => {
     
     var json = await response.Content.ReadAsStringAsync();
 
-    //TODO: Parse the response
+    // Begin Json transformation here 
 
     // Parse initial Json
     using var preParse = JsonDocument.Parse(json);
@@ -50,9 +52,12 @@ app.MapGet("/api/intraday/{symbol}", async (string symbol) => {
     if (root.TryGetProperty("Note", out var note))
         return Results.BadRequest(new { error = note.GetString() });
 
+    // Sometimes the API just fails and returns various error messages
     if (root.TryGetProperty("Error Message", out var errorMessage))
         return Results.BadRequest(new { error = errorMessage.GetString() });
 
+    // Mostly a defensive check. In reality API calls from this code *shouldn't*
+    // result in this. If it does it forwards the error to the new, locally hosted client.
     if (root.TryGetProperty("Information", out var info))
         return Results.BadRequest(new { error = info.GetString() });
     // End error handling
@@ -61,17 +66,17 @@ app.MapGet("/api/intraday/{symbol}", async (string symbol) => {
     if (!root.TryGetProperty($"Time Series ({timeInterval})", out var timeSeries))
         return Results.BadRequest(new { error = "No time series data found" });
 
-    // Dictionary keyed by day
+    // Dictionary keyed by day - entries are time specific
     var groupByDay = new Dictionary<string, (decimal lowSum, decimal highSum, long volumeSum, int count)>();
 
-    foreach (var kvp in timeSeries.EnumerateObject())
-    {
+    // Essentially, run through the fetched aplhavantage json and iterate through it, populating groupByDay
+    foreach (var kvp in timeSeries.EnumerateObject()){
 
         // set up timestamp sorting
         var timestamp = kvp.Name; // gives full timestamp with time included
         var day = timestamp.Substring(0, 10); //Strips the time from the timestamp YYYY-MM-DD
 
-        // set up each entry in the dictionary
+        // parse the entry (a time) for the values we need (low, high, volume)
         var entry = kvp.Value;
         var low = decimal.Parse(entry.GetProperty("3. low").GetString()!, CultureInfo.InvariantCulture);
         var high = decimal.Parse(entry.GetProperty("2. high").GetString()!, CultureInfo.InvariantCulture);
@@ -82,7 +87,9 @@ app.MapGet("/api/intraday/{symbol}", async (string symbol) => {
         groupByDay[day] = (0m, 0m, 0L, 0);
         }
 
-        // perform summation arithmetic
+        // perform summation arithmetic. Populate the day with starting values.
+        // Note: I could have averaged here, but the logic would have been more complex. I chose
+        // to use a count variable and calculate the avg outside of the loop for simplicity.
         var current = groupByDay[day];
         groupByDay[day] = (
             current.lowSum + low,
@@ -91,10 +98,10 @@ app.MapGet("/api/intraday/{symbol}", async (string symbol) => {
             current.count + 1);
     }
 
-    // perform final arithmetic
-    var results = groupByDay.OrderByDescending(kvp => kvp.Key).Select(kvp => new
-    {
-        
+    // perform final arithmetic. Average each day using the stored count of entries that day.
+    // Note: While count could theoretically be hardcoded due to X time always being in a given 
+    // day, defensive coding here was more practical.
+    var results = groupByDay.OrderByDescending(kvp => kvp.Key).Select(kvp => new {
         day = kvp.Key,
         lowAverage = Math.Round(kvp.Value.lowSum / kvp.Value.count, 4),
         highAverage = Math.Round(kvp.Value.highSum / kvp.Value.count, 4),
